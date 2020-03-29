8a350e6c2814313bbb421ab0de64b2250d16d2c5
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-03-30 00:06:43 +0430

    ADD config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config():
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+
+
+class Development(Config):
+    Debug = True
+
+class Production(Config):
+    DEBUG = False
