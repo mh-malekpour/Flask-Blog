2427457ab4acea460af06d0dc873a0c9964ccc6e
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-03-30 00:14:06 +0430

    Refactor

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Development
 
 
 app = Flask(__name__)
+app.config.from_object(Development)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 
 @app.route('/')
@@ -10,5 +18,7 @@ def index():
 
 
 from mod_admin import admin
+from mod_users import users
 
 app.register_blueprint(admin)
+app.register_blueprint(users)
