17b5ccb6a58377e76c34f2c7259443ac4b13e6bb
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-03-30 00:12:23 +0430

    Create users table in database

---
+++ mod_users/models.py
@@ -0,0 +1,15 @@
+from werkzeug.security import generate_password_hash
+from sqlalchemy import Column, Integer, String
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer(), primary_key=True)
+    email = Column(String(128), nullable=False, unique=True)
+    password = Column(String(128), nullable=False, unique=False)
+    role = Column(Integer(), nullable=False, default=0)
+    full_name = Column(String(128), nullable=True, unique=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
