b82e87e68c464a1447f692d408a51724a217a958
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-03-30 12:57:40 +0430

    Initialize admin/login view

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,6 +1,15 @@
+from flask import session
 from . import admin
 
 
 @admin.route('/')
 def index():
     return "Hello from admin Index"
+    
+
+@admin.route('/login')
+def login():
+    session['name'] = 'mohammad hossein'
+    print(session.get('name'))
+    # session.clear()
+    return 'Login Page'
