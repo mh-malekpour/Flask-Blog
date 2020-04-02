7e9994ae0f990a64dd3c3299848c84b5f5df3538
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-02 14:34:13 +0430

    Initialize register feature

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -2,9 +2,11 @@ from flask import session, render_template, request, abort, flash
 from mod_users.form import LoginForm
 from mod_users.models import User
 from . import admin
+from .utils import admin_only_view
 
 
 @admin.route('/')
+@admin_only_view
 def index():
     return "Hello from admin Index" 
     
@@ -22,9 +24,13 @@ def login():
         if not user.check_password(form.password.data):
             flash('Incorrect Credentials', category='error')
             return render_template('admin/login.html', form=form)
+        if not user.is_admin():
+            flash('Incorrect Credentials', category='error')
+            return render_template('admin/login.html', form=form)
         session['email'] = user.email
         session['user_id'] = user.id
+        session['role'] = user.role
         return "Logged in successfuly!"
-    if session.get('email') is not None:
+    if session.get('role') == 1:
         return "You are already logged in!"
     return render_template('admin/login.html', form=form)
--- mod_users/__init__.py
+++ mod_users/__init__.py
@@ -3,3 +3,4 @@ from flask import Blueprint
 users = Blueprint('users', __name__, url_prefix='/users/')
 
 from .models import User
+from . import views
