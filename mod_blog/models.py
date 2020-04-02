dae79a235b452e2159e84da8eafc7a062449eff4
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-03 00:59:25 +0430

    ADD posts_categories association table

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -1,13 +1,19 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, Table, ForeignKey
 from app import db
 
 
-class Categoty(db.Model):
+posts_categories = Table('posts_categories', db.metadata,
+    Column('post_id', Integer, ForeignKey('posts.id', ondelete='cascade')),
+    Column('categorie_id' , Integer, ForeignKey('categories.id', ondelete='cascade'))
+)
+
+class Category(db.Model):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False, unique=True) 
     description = Column(String(256), nullable=True, unique=False)
     slug = Column(String(128), nullable=False, unique=True)
+    posts = db.relationship('Post', secondary=posts_categories, back_populates='categories')
 
 
 
@@ -18,3 +24,4 @@ class Post(db.Model):
     summary = Column(String(256), nullable=True, unique=False)
     content = Column(Text, nullable=False, unique=False)
     slug = Column(String(128), nullable=False, unique=True)
+    categories = db.relationship('Category', secondary=posts_categories, back_populates='posts')
