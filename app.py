8062a2f95f1d39b5c3b8c454962b80b45b451486
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-02 18:31:01 +0430

    Register blog blue print

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ app.config.from_object(Development)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 from views import index
-
 
 from mod_admin import admin
 from mod_users import users
+from mod_blog import blog
 
 app.register_blueprint(admin)
 app.register_blueprint(users)
+app.register_blueprint(blog)
