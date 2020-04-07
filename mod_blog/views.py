376255f270cab38e9d0959434eaa80bbef1ead15
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-07 12:57:20 +0430

    show single post

--- mod_blog/views.py
+++ mod_blog/views.py
@@ -13,4 +13,4 @@ def index():
 @blog.route('/<string:slug>')
 def single_post(slug):
     post = Post.query.filter(Post.slug == slug).first()
-    return post.title
+    return render_template('blog/single_post.html', post=post)
